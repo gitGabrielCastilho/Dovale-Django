3f33ca73d93d0775f52997185c17d316783af690
Gabriel Castilho <gabrielmcastilho@gmail.com>
2023-12-31 13:56:29 -0300

    ignore teste

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,8 +148,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'email@gmail.com'
-EMAIL_HOST_PASSWORD = 'senha' 
+EMAIL_HOST_USER = 'teste.email.django.dvl@gmail.com'
+EMAIL_HOST_PASSWORD = 'xarboyeafkfeztix' 
 EMAIL_USE_TLS = True
 
 SECURE_BROWSER_XSS_FILTER = True
