54e817901dba164783e8339adc587a3d76439480
Gabriel Castilho <gabrielmcastilho@gmail.com>
2023-12-31 13:29:51 -0300

    gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,8 +148,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'email@gmail.com'
-EMAIL_HOST_PASSWORD = 'app-password'
+EMAIL_HOST_USER = 'gabrielmcastilho@gmail.com'
+EMAIL_HOST_PASSWORD = 'gtatfocqzhblixfq'
 EMAIL_USE_TLS = True
 
 SECURE_BROWSER_XSS_FILTER = True
